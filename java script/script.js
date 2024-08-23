d20453b226eee76167ac6ee30e5332e8d3045b9d
Mrashed369 <mohamedrashed663@gmail.com>
2024-08-23 17:24:37 +0300

    Add files via upload

---
+++ java script/script.js
@@ -0,0 +1,60 @@
+
+
+
+// var allproducts = document.querySelectorAll(".card")
+// var div1 = document.querySelector("#div1")
+// var btn1 = document.querySelector("#btn1price")
+// var totalPrice = 0
+// var div2 =document.querySelector("#div2")
+
+// allproducts.forEach(function (item) {
+//     item.onclick = function (){
+//         totalPrice += + (item.getAttribute("Price"))
+//         div1.innerHTML += item.textContent + "-"
+
+//         if (div1.innerHTML !=""){
+//             btn1.style.display ="block";
+//             btn1.style.background ="blue"
+//             btn1.style.width="1000px"
+//             btn1.style.height ="50px"
+//             btn1.style.color ="white"
+//             btn1.style.margin ="auto"
+//         }
+//     }
+// })
+
+
+// btn1.onclick = function (item){
+//     div2.innerHTML = (totalPrice)
+
+//     div2.style.display ="block";
+//     div2.style.background ="black"
+//     div2.style.color ="white"
+//     div2.style.width="1000px"
+//     div2.style.height ="50px"
+//     div2.style.color ="white"
+//     div2.style.margin ="auto"
+
+
+
+    // div1.style.display ="block";
+    // div1.style.background ="red"
+    // div1.style.color ="white"
+    // div1.style.width="1000px"
+    // div1.style.height ="50px"
+    // div1.style.color ="white"
+    // div1.style.margin ="auto"
+
+
+
+
+
+
+// }
+
+
+
+
+
+
+
